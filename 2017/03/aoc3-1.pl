a3a71e0c09f9e01d3586e33867c19b7232fc8f1e
Christer Boräng <mort@chalmers.se>
2020-07-25 00:25:03 +0200

    Not much happening here

---
+++ 2017/03/aoc3-1.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/env perl
+# Copyright Christer Boräng (mort@chalmers.se) 2019
+
+# Always use strict and warnings
+use strict;
+use warnings;
+no warnings 'recursion';
+use Data::Dumper;
+use List::Util qw( max min );
+
+print "Solved using calculator\n";
+exit;
+
+{
+    my $file = shift @ARGV;
+    open(my $fh, '<', $file) or die "Can't open file $file";
+    @rows = <$fh>;
+    close($fh);
+    chomp @rows;
+}
+
+sub D
+{
+    my $str = shift;
+    print Dumper($str);
+}
+
+# Other modules
+
+# Global variables
+
+
+__END__
+
+=head1 NAME
+
+<application name> - one line description
+
+
+=head1 VERSION
+
+This documentation refers to <application name> version 0.0.1
+
+
+=head1 USAGE
+
+
+=head1 REQUIRED ARGUMENTS
+
+
+=head1 OPTIONS
+
+
+=head1 DESCRIPTION
+
+
+=head1 DIAGNOSTICS
+
+
+=head1 EXIT STATUS
+
+=head1 CONFIGURATION
+
+
+=head1 DEPENDENCIES
+=head1 INCOMPATIBILITIES
+=head1 BUGS AND LIMITATIONS
+=head1 AUTHOR
+
+Written by Christer Boräng (mort@chalmers.se)
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright Christer Boräng 2019
+
+=cut
