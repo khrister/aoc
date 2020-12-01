9c7cf22a5824049b5bccd491a851132912b0f458
Christer Boräng <mort@chalmers.se>
2020-12-01 11:45:34 +0100

    2020 day 1 problems sorted

---
+++ 2020/01/aoc1.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/env perl
+# Copyright Christer Boräng (mort@chalmers.se) 2019
+
+# Always use strict and warnings
+use strict;
+use warnings;
+no warnings 'recursion';
+use Data::Dumper;
+use List::Util 'max';
+use List::MoreUtils qw (first_index);
+
+my @expenses;
+
+{
+    die "Usage: $0 <file>" unless (@ARGV == 1);
+    my $file = shift @ARGV;
+
+    my $fh;
+    open($fh, '<', $file);
+    @expenses = <$fh>;
+    close($fh);
+    chomp(@expenses);
+}
+
+foreach my $exp (@expenses)
+{
+#    print "Checking $exp\n";
+
+    foreach my $exp2 (@expenses)
+    {
+        if ($exp + $exp2 == 2020)
+        {
+            print "$exp + $exp2 = 2020, $exp * $exp2 = " .
+                ($exp * $exp2) . "\n";
+            exit;
+        }
+    }
+}
+
+# Debug function
+sub D
+{
+    my $str = shift;
+    print Dumper($str);
+}
+
+# Other modules
+
+# Global variables
+
+
+__END__
+
+=head1 NAME
+
+<application name> - one line description
+
+
+=head1 VERSION
+
+This documentation refers to <application name> version 0.0.1
+
+
+=head1 USAGE
+
+
+=head1 REQUIRED ARGUMENTS
+
+
+=head1 OPTIONS
+
+
+=head1 DESCRIPTION
+
+
+=head1 DIAGNOSTICS
+
+
+=head1 EXIT STATUS
+
+=head1 CONFIGURATION
+
+
+=head1 DEPENDENCIES
+=head1 INCOMPATIBILITIES
+=head1 BUGS AND LIMITATIONS
+=head1 AUTHOR
+
+Written by Christer Boräng (mort@chalmers.se)
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright Christer Boräng 2019
+
+=cut
