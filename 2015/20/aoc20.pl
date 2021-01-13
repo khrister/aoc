247848e435a31a8345b7d5fdb7412dadef1be0d4
Christer Boräng <mort@chalmers.se>
2021-01-13 22:48:27 +0100

    Cleaned up the code

--- 2015/20/aoc20.pl
+++ 2015/20/aoc20.pl
@@ -1,1 +1,80 @@
-use feature "say"; use Math::Prime::Util qw(divisor_sum); $i=1; while (1) { $a =  divisor_sum($i); if ($a >= 3310000) { say "$i  $a"; exit; } $i++}
+#!/usr/bin/env perl
+# Copyright Christer Boräng (mort@chalmers.se) 2019
+
+# Always use strict and warnings
+use strict;
+use warnings;
+no warnings 'recursion';
+use feature 'say';
+
+# Other modules
+use Data::Dumper;
+use Math::Prime::Util qw( divisor_sum );
+
+# Global variables
+my $i = 1;
+
+while (1)
+{
+    my $a =  divisor_sum($i);
+    if ($a >= 3310000)
+    {
+        say "$i  $a";
+        exit;
+    }
+    $i++;
+}
+
+# Debug function
+sub D
+{
+    my $str = shift;
+    print Dumper($str);
+}
+
+
+__END__
+
+=head1 NAME
+
+<application name> - one line description
+
+
+=head1 VERSION
+
+This documentation refers to <application name> version 0.0.1
+
+
+=head1 USAGE
+
+
+=head1 REQUIRED ARGUMENTS
+
+
+=head1 OPTIONS
+
+
+=head1 DESCRIPTION
+
+
+=head1 DIAGNOSTICS
+
+
+=head1 EXIT STATUS
+
+=head1 CONFIGURATION
+
+
+=head1 DEPENDENCIES
+=head1 INCOMPATIBILITIES
+=head1 BUGS AND LIMITATIONS
+=head1 AUTHOR
+
+Written by Christer Boräng (mort@chalmers.se)
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright Christer Boräng 2019
+
+=cut
