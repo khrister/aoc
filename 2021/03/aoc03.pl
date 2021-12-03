f5b447f177bd9c0c6be3dc4b98d9448512a99133
Christer Boräng <mort@chalmers.se>
2021-12-03 20:37:34 +0100

    dag 3 del 1

---
+++ 2021/03/aoc03.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/env perl
+# Copyright Christer Boräng (mort@chalmers.se) 2019
+
+# Always use strict and warnings
+use strict;
+use warnings;
+no warnings 'recursion';
+use Data::Dumper;
+use List::Util qw (max sum);
+use List::MoreUtils qw (first_index);
+
+my @numbers;
+my @ones;
+
+{
+    die "Usage: $0 <file>" unless (@ARGV == 1);
+    my $file = shift @ARGV;
+
+    my $fh;
+    open($fh, '<', $file);
+    @numbers = <$fh>;
+    chomp @numbers;
+    close($fh);
+}
+
+#D(\@numbers);
+
+foreach my $num (@numbers)
+{
+    my @bits;
+    @bits = split(//, $num);
+    for (my $i = 0; $i <= $#bits; $i++)
+    {
+        $ones[$i] += $bits[$i];
+    }
+}
+
+D(\@ones);
+
+my $gamma = "0b";
+my $epsilon = "0b";
+
+foreach my $digit (@ones)
+{
+    if ($digit > (@numbers / 2))
+    {
+        $gamma .= 1;
+        $epsilon .= 0;
+    }
+    else
+    {
+        $gamma .= 0;
+        $epsilon .= 1;
+    }
+}
+
+print "Power Consumption: " . (oct($gamma) * oct($epsilon)) . "\n";
+
+# Debug function
+sub D
+{
+    my $str = shift;
+    print Dumper($str);
+}
+
+# Other modules
+
+# Global variables
+
+
+__END__
+
+=head1 NAME
+
+<application name> - one line description
+
+
+=head1 VERSION
+
+This documentation refers to <application name> version 0.0.1
+
+
+=head1 USAGE
+
+
+=head1 REQUIRED ARGUMENTS
+
+
+=head1 OPTIONS
+
+
+=head1 DESCRIPTION
+
+
+=head1 DIAGNOSTICS
+
+
+=head1 EXIT STATUS
+
+=head1 CONFIGURATION
+
+
+=head1 DEPENDENCIES
+=head1 INCOMPATIBILITIES
+=head1 BUGS AND LIMITATIONS
+=head1 AUTHOR
+
+Written by Christer Boräng (mort@chalmers.se)
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright Christer Boräng 2019
+
+=cut
