03486df0944c4c80f88e8f3d2f60ae298cd9a9d0
Christer Boräng <mort@chalmers.se>
2023-12-15 15:16:56 +0100

    2023 15:1 done

---
+++ 2023/15/aoc15.pl
@@ -0,0 +1,100 @@
+#!/usr/bin/env perl
+# Copyright Christer Boräng (mort@chalmers.se) 2023
+
+# Always use strict and warnings
+use strict;
+use warnings;
+no warnings 'recursion';
+use Data::Dumper;
+
+# Other modules
+use List::Util qw (max sum);
+use List::MoreUtils qw (first_index);
+use Array::Utils qw(:all);
+
+# Global variables
+my $sum;
+
+{
+    die "Usage: $0 <file>" unless (@ARGV == 1);
+    my $file = shift @ARGV;
+
+    my $fh;
+    open($fh, '<', $file);
+    my $line = <$fh>;
+    chomp $line;
+    close($fh);
+
+    foreach my $str (split(/,/, $line))
+    {
+        my @chars = split(//, $str);
+        my $cur = 0;
+        #print "cur: $cur\n";
+        foreach my $char (@chars)
+        {
+            $cur += ord($char);
+            $cur *= 17;
+            #print "$char: $cur\n";
+        }
+        $cur %= 256;
+        $sum += $cur;
+    }
+}
+
+print "$sum\n";
+
+
+
+# Debug function
+sub D
+{
+    my $str = shift;
+    print Dumper($str);
+}
+
+
+__END__
+
+=head1 NAME
+
+<application name> - one line description
+
+
+=head1 VERSION
+
+This documentation refers to <application name> version 0.0.1
+
+
+=head1 USAGE
+
+
+=head1 REQUIRED ARGUMENTS
+
+
+=head1 OPTIONS
+
+
+=head1 DESCRIPTION
+
+
+=head1 DIAGNOSTICS
+
+
+=head1 EXIT STATUS
+
+=head1 CONFIGURATION
+
+
+=head1 DEPENDENCIES
+=head1 INCOMPATIBILITIES
+=head1 BUGS AND LIMITATIONS
+=head1 AUTHOR
+
+Written by Christer Boräng (mort@chalmers.se)
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright Christer Boräng 2023
+
+=cut
