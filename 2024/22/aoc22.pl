dd7c953ea5c6c254257fae3f779704e0f734d155
Christer Boräng <khrister@gmail.com>
2024-12-22 14:36:50 +0100

    2024 day 22 part 1 done

---
+++ 2024/22/aoc22.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/env perl
+# Copyright Christer Boräng (mort@chalmers.se) 2023
+
+# Always use strict and warnings
+use strict;
+use warnings;
+no warnings 'recursion';
+use Data::Dumper;
+use feature 'say';
+
+# Other modules
+use List::Util qw (max sum);
+use List::MoreUtils qw (first_index);
+use Array::Utils qw(:all);
+use Array::Compare;
+use Algorithm::Combinatorics qw (combinations);;
+
+# Global variables
+
+{
+    die "Usage: $0 <file>" unless (@ARGV == 1);
+    my $file = shift @ARGV;
+
+    my $fh;
+    open($fh, '<', $file);
+
+    my $sum = 0;
+    while (my $secret = <$fh>)
+    {
+        chomp $secret;
+
+        for (1..2000)
+        {
+            $secret = (($secret * 64) ^ $secret) % 16777216;
+            $secret = (int($secret / 32) ^ $secret) % 16777216;
+            $secret = (($secret * 2048) ^ $secret) % 16777216;
+        }
+        $sum += $secret;
+    }
+    close($fh);
+    say $sum;
+}
+
+
+# Debug function
+sub D
+{
+    my $str = shift;
+    print Dumper($str);
+}
+
+
+__END__
+
+=head1 NAME
+
+<application name> - one line description
+
+
+=head1 VERSION
+
+This documentation refers to <application name> version 0.0.1
+
+
+=head1 USAGE
+
+
+=head1 REQUIRED ARGUMENTS
+
+
+=head1 OPTIONS
+
+
+=head1 DESCRIPTION
+
+
+=head1 DIAGNOSTICS
+
+
+=head1 EXIT STATUS
+
+=head1 CONFIGURATION
+
+
+=head1 DEPENDENCIES
+=head1 INCOMPATIBILITIES
+=head1 BUGS AND LIMITATIONS
+=head1 AUTHOR
+
+Written by Christer Boräng (mort@chalmers.se)
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright Christer Boräng 2023
+
+=cut
